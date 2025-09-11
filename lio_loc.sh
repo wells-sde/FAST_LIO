6a94ee6ad7630222e6ad7b1421e20a3419162659
will <hhheshui@126.com>
2025-09-11 16:57:17 +0800

    修改slam启动脚本，增加点云文件路径参数

--- lio_loc.sh
+++ lio_loc.sh
@@ -1,18 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
+
+# Check if the file path argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <pcd_path>"
+    exit 1
+fi
+
 
 #设置为实际的ros workspace根目录
 WORK_DIR=$(dirname "$(dirname "$(pwd)")")
 
-#预建点云地图文件路径
-MAPFILE_PATH="$(pwd)/PCD/filterGlobalMap.pcd"
+#预建点云地图文件路径 filterGlobalMap.pcd
+MAPFILE_PATH=$1
 
 # 启动SLAM
-gnome-terminal -t "LIO" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch fast_lio locallization_unitree_G1.launch map_file:=$MAPFILE_PATH;exec bash;"
-sleep 2s
+gnome-terminal -t "LIO" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch fast_lio localization_unitree_G1.launch map_file:=$MAPFILE_PATH;exec bash;"
+sleep 3s
 
 # 启动Lidar
 gnome-terminal -t "Livox driver" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch livox_ros_driver2 msg_MID360.launch;exec bash;"
-sleep 2s
-
-# 启动OctoMapping
-# gnome-terminal -t "octo_mapping" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch fast_lio octo_mapping.launch;exec bash;"
--- lio_mapping.sh
+++ lio_mapping.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
 
 #设置为实际的ros workspace根目录
 WORK_DIR=$(dirname "$(dirname "$(pwd)")")
@@ -7,9 +7,9 @@ WORK_DIR=$(dirname "$(dirname "$(pwd)")")
 gnome-terminal -t "LIO" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch fast_lio mapping_unitree_G1.launch;exec bash;"
 sleep 2s
 
-# 启动Lidar
-gnome-terminal -t "Livox driver" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch livox_ros_driver2 msg_MID360.launch;exec bash;"
-sleep 2s
-
 # 启动OctoMapping
 gnome-terminal -t "octo_mapping" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch fast_lio octo_mapping.launch;exec bash;"
+sleep 2s
+
+# 启动Lidar
+gnome-terminal -t "Livox driver" --working-directory="$WORK_DIR"  -- bash -c  "source ./devel/setup.bash;roslaunch livox_ros_driver2 msg_MID360.launch;exec bash;"
